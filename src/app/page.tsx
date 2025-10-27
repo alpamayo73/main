ce010105e3574bab88e0e110c85bbb19f54f7517
alpamayo73 <alpamayo73@gmail.com>
2025-10-27 19:57:29 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import Head from 'next/head';
+import Hero from '../components/home/Hero';
+import FeaturedServices from '../components/home/FeaturedServices';
+import Features from '../components/home/Features';
+import AboutPreview from '../components/home/AboutPreview';
+import CTASection from '../components/home/CTASection';
 
-
-
-export default async function Home() {
+export default function Home() {
   return (
     <>
+      <Head>
+        <title>Alpamayo Technical Services | Premium Renovation & AC Services in Dubai</title>
+        <meta name="description" content="Premium renovation and AC services in Dubai. Villa, apartment, office renovation and professional AC system installation and maintenance." />
+      </Head>
 
-
+      <Hero />
+      <Features />
+      <FeaturedServices />
+      <AboutPreview />
+      <CTASection />
     </>
   );
 }
