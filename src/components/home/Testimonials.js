4e1b3adda8a31aca6bc123d2b294a98068baa3cd
alpamayo73 <alpamayo73@gmail.com>
2025-10-28 01:41:23 +0500

    Update Testimonials.js

--- src/components/home/Testimonials.js
+++ src/components/home/Testimonials.js
@@ -1,5 +1,5 @@
 // src/components/home/Testimonials.js
-import { Star } from 'lucide-react'
+import Image from 'next/image'
 
 export default function Testimonials() {
   const testimonials = [
@@ -44,7 +44,9 @@ export default function Testimonials() {
             <div key={testimonial.id} className="bg-white p-8 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
               <div className="flex items-center mb-4">
                 {[...Array(testimonial.rating)].map((_, i) => (
-                  <Star key={i} size={20} className="text-yellow-400 fill-current" />
+                  <svg key={i} className="w-5 h-5 text-yellow-400 fill-current" viewBox="0 0 20 20">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
                 ))}
               </div>
               
