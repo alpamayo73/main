4c28288af15b714978e0fe81da6e54822469fed5
alpamayo73 <alpamayo73@gmail.com>
2025-11-05 01:44:10 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
       <Stats />
       <LuxuryServices />
       <Process />
-      <PortfolioShowcase />
+      
       <Testimonials />
       <CTASection />
       <ContactPreview />
